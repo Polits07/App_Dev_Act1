b9a4c805fe1e9cb1a8548d00946404c20a87041e
Hipolito <jnistianhipolitov7@gmail.com>
2025-06-12 16:55:27 +0700

    Adding New Sub "Ethics"

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ namespace Activity1
 {
     class Grading
     {
-        static float Compute(int Programming, int Multimedia, int Electives)
+        static float Compute(int Programming, int Multimedia, int Electives, int Ethics)
         {
-            int sum = (Programming + Multimedia + Electives);
-            float Ave = (float)sum / 3;
+            int sum = (Programming + Multimedia + Electives + Ethics);
+            float Ave = (float)sum / 4;
             return Ave;
         }
         static void Main(String[] args)
         {
-            int programming, multimedia, electives;
+            int programming, multimedia, electives, ethics;
 
             Console.Write("Enter grade in Programming: ");
             programming = Convert.ToInt32(Console.ReadLine());
@@ -23,7 +23,10 @@ namespace Activity1
             Console.Write("Enter grade in Electives: ");
             electives = Convert.ToInt32(Console.ReadLine());
 
-            float Ave = Compute(programming, multimedia, electives);
+            Console.Write("Enter grade in Ethics: ");
+            ethics = Convert.ToInt32(Console.ReadLine());
+
+            float Ave = Compute(programming, multimedia, electives, ethics);
 
             Console.WriteLine("  ");
             Console.WriteLine($"Final Grade: {Ave:F2}");
