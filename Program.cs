0fae63f2cc318ea30748dd624ed103898140d23f
Hipolito <jnistianhipolitov7@gmail.com>
2025-06-12 16:49:49 +0700

    Changing Sub

--- Program.cs
+++ Program.cs
@@ -4,29 +4,26 @@ namespace Activity1
 {
     class Grading
     {
-        static float Compute(int Science, int Math, int English, int Filipino)
+        static float Compute(int Programming, int Multimedia, int Electives)
         {
-            int sum = (Science + Math + English + Filipino);
-            float Ave = (float)sum / 4;
+            int sum = (Programming + Multimedia + Electives);
+            float Ave = (float)sum / 3;
             return Ave;
         }
         static void Main(String[] args)
         {
-            int science, math, english, filipino;
+            int programming, multimedia, electives;
 
-            Console.Write("Enter grade in Science: ");
-            science = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter grade in Programming: ");
+            programming = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter grade in Math: ");
-            math = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter grade in Multimedia: ");
+            multimedia = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter grade in English: ");
-            english = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter grade in Electives: ");
+            electives = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter grade in Filipino: ");
-            filipino = Convert.ToInt32(Console.ReadLine());
-
-            float Ave = Compute(science, math, english, filipino);
+            float Ave = Compute(programming, multimedia, electives);
 
             Console.WriteLine("  ");
             Console.WriteLine($"Final Grade: {Ave:F2}");
